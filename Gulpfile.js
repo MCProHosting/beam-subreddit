29973c3be914f29a2139b4ebadd54bfb3ee0e77e
Taylor Blau <me@ttaylorr.com>
2015-03-11 20:04:58 -0400

    Create reddit_upload Gulp task, include example config

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp')
-  , sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    reddit = require('./lib/reddit_upload.js');
 
 gulp.task('dist', function () {
     gulp.src('./assets/stylesheets/**/*.scss')
@@ -8,3 +9,10 @@ gulp.task('dist', function () {
         }))
         .pipe(gulp.dest('./dist/stylesheets'));
 });
+
+gulp.task('push', function (done) {
+    return gulp.src('./dist/stylesheets/index.css')
+               .pipe(reddit())
+});
+
+gulp.task('default', ['dist', 'push']);
---
+++ config/config.example.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dest: '/r/<subreddit>',
+    app: {
+        token: "<token>",
+        secret: "<secret>"
+    },
+    user: {
+        username: "<username>",
+        password: "<password>"
+    }
+}
